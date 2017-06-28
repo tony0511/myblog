b178a9e4bebb33953c856eacf640ad2b8097e676
tony0511 <zhuimengzhe91@163.com>
2017-06-28 11:33:38 +0800

    ddssd

---
+++ html/qqbbsc/static/data.js
@@ -0,0 +1,580 @@
+module.exports = {
+  "gc": {
+    "getgoodtype": [
+      {
+        "styleId": 1,
+        "styleName": "家电数码"
+      },
+      {
+        "styleId": 2,
+        "styleName": "饰品配件"
+      },
+      {
+        "styleId": 3,
+        "styleName": "美食"
+      },
+      {
+        "styleId": 4,
+        "styleName": "男装"
+      },
+      {
+        "styleId": 5,
+        "styleName": "女装"
+      },
+      {
+        "styleId": 6,
+        "styleName": "鞋靴"
+      },
+      {
+        "styleId": 7,
+        "styleName": "户外健身"
+      },
+      {
+        "styleId": 8,
+        "styleName": "箱包"
+      },
+      {
+        "styleId": 9,
+        "styleName": "童装玩具"
+      },
+      {
+        "styleId": 10,
+        "styleName": "乐器"
+      }
+    ],
+    "getgood": {
+      "freeDelivery": "1",
+      "goodDesc": "妙雀咖啡机商用全自动咖啡饮料机冷热 速溶咖啡奶茶一体机热饮机",
+      "goodDiscountedPrice": 3380,
+      "goodId": 1,
+      "goodName": "咖啡机",
+      "goodOriginalPrice": 4280,
+      "goodPicPath": "https://gd1.alicdn.com/imgextra/i4/2804539712/TB2DtsWqRNkpuFjy0FaXXbRCVXa_!!2804539712.jpg",
+      "goodStatus": "1",
+      "salsNum": 68,
+      "serviceType": [
+        "七天无理由",
+        "运费险",
+        "换单包赔",
+        "超值包邮"
+      ],
+      "specification": "台",
+      "styleId": 1
+    },
+    "getallgood": [
+      {
+        "freeDelivery": "1",
+        "goodDesc": "妙雀咖啡机商用全自动咖啡饮料机冷热 速溶咖啡奶茶一体机热饮机",
+        "goodDiscountedPrice": 4280,
+        "goodId": 1,
+        "goodName": "咖啡机",
+        "goodOriginalPrice": 4280,
+        "goodPicPath": "https://gd1.alicdn.com/imgextra/i4/2804539712/TB2DtsWqRNkpuFjy0FaXXbRCVXa_!!2804539712.jpg",
+        "goodStatus": "",
+        "salsNum": 68,
+        "serviceType": [],
+        "specification": "台",
+        "styleId": 1
+      },
+      {
+        "freeDelivery": "1",
+        "goodDesc": "发烧hifi书架音箱三四分频10寸木质监听无源音响震撼低音SP高保真",
+        "goodDiscountedPrice": 1814.4,
+        "goodId": 2,
+        "goodName": "HIFI音箱",
+        "goodOriginalPrice": 2880,
+        "goodPicPath": "https://g-search1.alicdn.com/img/bao/uploaded/i4/imgextra/i4/198000345864446200/TB2Nnwlbb0kpuFjy0FjXXcBbVXa_!!0-saturn_solar.jpg_230x230.jpg_.webp",
+        "goodStatus": "",
+        "salsNum": 46,
+        "serviceType": [],
+        "specification": "对",
+        "styleId": 1
+      },
+      {
+        "freeDelivery": "1",
+        "goodDesc": "乐焙芝电烤箱 家用烘焙 多功能 蛋糕面包 旋转烤叉 炉灯 披萨烤箱",
+        "goodDiscountedPrice": 1238,
+        "goodId": 3,
+        "goodName": "烤箱",
+        "goodOriginalPrice": 4999,
+        "goodPicPath": "https://g-search2.alicdn.com/img/bao/uploaded/i4/i4/TB1jEJlRFXXXXa8XXXXXXXXXXXX_!!0-item_pic.jpg_230x230.jpg_.webp",
+        "goodStatus": "",
+        "salsNum": 6,
+        "serviceType": [],
+        "specification": "台",
+        "styleId": 1
+      },
+      {
+        "freeDelivery": "1",
+        "goodDesc": "斯贝伊诺扫地机器人家用一体机 超薄智能自动洗擦地机拖地吸尘器",
+        "goodDiscountedPrice": 1580,
+        "goodId": 4,
+        "goodName": "吸尘器",
+        "goodOriginalPrice": 5999,
+        "goodPicPath": "https://g-search1.alicdn.com/img/bao/uploaded/i4/i3/TB1yesHKVXXXXceXVXXXXXXXXXX_!!0-item_pic.jpg_230x230.jpg_.webp",
+        "goodStatus": "",
+        "salsNum": 6,
+        "serviceType": [],
+        "specification": "台",
+        "styleId": 1
+      }
+    ],
+    "goodimage": [
+      {
+        "goodId": 1,
+        "imageId": 2,
+        "picPath": "https://gd1.alicdn.com/imgextra/i4/2804539712/TB2DtsWqRNkpuFjy0FaXXbRCVXa_!!2804539712.jpg",
+        "picStatus": "1"
+      },
+      {
+        "goodId": 1,
+        "imageId": 3,
+        "picPath": "https://gd4.alicdn.com/imgextra/i4/2804539712/TB25GMRXg_xQeBjy0FoXXX1vpXa_!!2804539712.jpg",
+        "picStatus": "1"
+      },
+      {
+        "goodId": 1,
+        "imageId": 4,
+        "picPath": "https://gd1.alicdn.com/imgextra/i1/2804539712/TB2XSkTsEhnpuFjSZFEXXX0PFXa_!!2804539712.jpg",
+        "picStatus": "1"
+      },
+      {
+        "goodId": 1,
+        "imageId": 5,
+        "picPath": "https://gd4.alicdn.com/imgextra/i4/2804539712/TB2wPsVsxBmpuFjSZFDXXXD8pXa_!!2804539712.jpg",
+        "picStatus": "1"
+      },
+      {
+        "goodId": 1,
+        "imageId": 6,
+        "picPath": "https://gd2.alicdn.com/imgextra/i2/2804539712/TB2Bd8Kq3RkpuFjy1zeXXc.6FXa_!!2804539712.jpg",
+        "picStatus": "1"
+      }
+    ]
+  },
+  "tic": {
+    "turnimage": [
+      {
+        "imageLink": "gc/getgood?goodId=8",
+        "imagePath": "https://img.alicdn.com/simba/img/TB1HH6HRVXXXXaoXVXXSutbFXXX.jpg",
+        "imageStatus": "1",
+        "turnId": 1,
+        "turnTitle": "雷蛇笔记本电脑"
+      },
+      {
+        "imageLink": "gc/getgood?goodId=6",
+        "imagePath": "https://aecpm.alicdn.com/tfscom/TB15dYNRVXXXXc2XFXXXXXXXXXX.jpg",
+        "imageStatus": "1",
+        "turnId": 2,
+        "turnTitle": "连衣裙"
+      },
+      {
+        "imageLink": "gc/getgood?goodId=4",
+        "imagePath": "https://img.alicdn.com/simba/img/TB1nZatRFXXXXXiapXXSutbFXXX.jpg",
+
+        "imageStatus": "1",
+        "turnId": 3,
+        "turnTitle": "吸尘器"
+      }
+    ]
+  },
+  "order": {
+    "showorderinfo": [
+      {
+        "deadline": "2017-06-16 19:29:14",
+        "expressSelect": {
+          "express_name": "快递",
+          "express_status": "0",
+          "expressid": 1
+        },
+        "frontid": 1,
+        "message": "赠品要红色的",
+        "orderItem": [
+          {
+            "detailid": 6,
+            "goodInfo": {
+              "freeDelivery": "免费配送",
+              "goodDesc": "三国演义刘关张",
+              "goodDiscountedPrice": 62.88,
+              "goodId": 1,
+              "goodName": "三国演义",
+              "goodOriginalPrice": 98.00,
+              "goodPicPath": "https://img.alicdn.com/bao/uploaded/i3/TB1h1qdRXXXXXX3XXXXXXXXXXXX_!!0-item_pic.jpg_430x430q90.jpg",
+              "goodStatus": "1",
+              "salsNum": 100,
+              "specification": "啊积分卡师傅",
+              "styleId": 1
+            },
+            "orderid": 6,
+            "price": 10,
+            "quantity": 1,
+            "status": 1
+          },
+          {
+            "detailid": 7,
+            "goodInfo": {
+              "freeDelivery": "免费配送",
+              "goodDesc": "红楼梦四大家族",
+              "goodDiscountedPrice": 89.99,
+              "goodId": 2,
+              "goodName": "红楼梦",
+              "goodOriginalPrice": 188.00,
+              "goodPicPath": "https://img.alicdn.com/bao/uploaded/i4/TB1XC1pKVXXXXbmXFXXXXXXXXXX_!!0-item_pic.jpg_430x430q90.jpg",
+              "goodStatus": "1",
+              "salsNum": 100,
+              "specification": "啊积分卡师傅",
+              "styleId": 1
+            },
+            "orderid": 6,
+            "price": 10,
+            "quantity": 1,
+            "status": 1
+          }
+        ],
+        "orderid": 6,
+        "orderstatus": 1,
+        "ordertime": "2017-06-15 19:29:14",
+        "receipt": {
+          "addressinfo": "海外冰火岛",
+          "area": null,
+          "phone": "1274148173113",
+          "pid": 1,
+          "pname": "谢逊",
+          "status": "0",
+          "userid": 1
+        },
+        "totalnum": 100,
+        "totalprice": 89
+      }
+    ]
+  },
+  "qbuid": [
+    {
+      "addressinfo": "八一街北二巷78号12栋",
+      "area": {
+        "area": "靖安区",
+        "areaID": "210401",
+        "city": {
+          "city": "抚顺市",
+          "cityID": "210400",
+          "id": 42,
+          "province": {
+            "id": 6,
+            "province": "辽宁省",
+            "provinceID": "210000"
+          }
+        },
+        "id": 492
+      },
+      "phone": "15182316532",
+      "pid": 6,
+      "pname": "张晓峰",
+      "status": "0",
+      "userid": 2
+    },
+    {
+      "addressinfo": "禾市镇前进路46号2栋805",
+      "area": {
+        "area": "泰和县",
+        "areaID": "210401",
+        "city": {
+          "city": "吉安市",
+          "cityID": "210400",
+          "id": 42,
+          "province": {
+            "id": 6,
+            "province": "江西省",
+            "provinceID": "210000"
+          }
+        },
+        "id": 492
+      },
+      "phone": "18662834322",
+      "pid": 7,
+      "pname": "黄甜甜",
+      "status": "1",
+      "userid": 2
+    },
+    {
+      "addressinfo": "五环东乡街三里巷34号67栋",
+      "area": {
+        "area": "海淀区",
+        "areaID": "210401",
+        "city": {
+          "city": "北京市",
+          "cityID": "210400",
+          "id": 42,
+          "province": {
+            "id": 6,
+            "province": "北京",
+            "provinceID": "210000"
+          }
+        },
+        "id": 492
+      },
+      "phone": "13328786569",
+      "pid": 9,
+      "pname": "贺翔",
+      "status": "0",
+      "userid": 2
+    }
+  ],
+  "getProvince": [
+    {
+      "id": 1,
+      "province": "北京市",
+      "provinceID": "110000"
+    },
+    {
+      "id": 2,
+      "province": "天津市",
+      "provinceID": "120000"
+    }
+  ],
+  "getCityByFather": [
+    {
+      "city": "石家庄市",
+      "cityID": "130100",
+      "id": 5,
+      "province": null
+    },
+    {
+      "city": "唐山市",
+      "cityID": "130200",
+      "id": 6,
+      "province": null
+    },
+    {
+      "city": "秦皇岛市",
+      "cityID": "130300",
+      "id": 7,
+      "province": null
+    },
+    {
+      "city": "邯郸市",
+      "cityID": "130400",
+      "id": 8,
+      "province": null
+    },
+    {
+      "city": "邢台市",
+      "cityID": "130500",
+      "id": 9,
+      "province": null
+    },
+    {
+      "city": "保定市",
+      "cityID": "130600",
+      "id": 10,
+      "province": null
+    },
+    {
+      "city": "张家口市",
+      "cityID": "130700",
+      "id": 11,
+      "province": null
+    },
+    {
+      "city": "承德市",
+      "cityID": "130800",
+      "id": 12,
+      "province": null
+    },
+    {
+      "city": "沧州市",
+      "cityID": "130900",
+      "id": 13,
+      "province": null
+    },
+    {
+      "city": "廊坊市",
+      "cityID": "131000",
+      "id": 14,
+      "province": null
+    },
+    {
+      "city": "衡水市",
+      "cityID": "131100",
+      "id": 15,
+      "province": null
+    }
+  ],
+  "getAreaByFather": [
+    {
+      "area": "东城区",
+      "areaID": "110101",
+      "city": null,
+      "id": 1
+    },
+    {
+      "area": "西城区",
+      "areaID": "110102",
+      "city": null,
+      "id": 2
+    },
+    {
+      "area": "崇文区",
+      "areaID": "110103",
+      "city": null,
+      "id": 3
+    },
+    {
+      "area": "宣武区",
+      "areaID": "110104",
+      "city": null,
+      "id": 4
+    },
+    {
+      "area": "朝阳区",
+      "areaID": "110105",
+      "city": null,
+      "id": 5
+    },
+    {
+      "area": "丰台区",
+      "areaID": "110106",
+      "city": null,
+      "id": 6
+    },
+    {
+      "area": "石景山区",
+      "areaID": "110107",
+      "city": null,
+      "id": 7
+    },
+    {
+      "area": "海淀区",
+      "areaID": "110108",
+      "city": null,
+      "id": 8
+    },
+    {
+      "area": "门头沟区",
+      "areaID": "110109",
+      "city": null,
+      "id": 9
+    },
+    {
+      "area": "房山区",
+      "areaID": "110111",
+      "city": null,
+      "id": 10
+    },
+    {
+      "area": "通州区",
+      "areaID": "110112",
+      "city": null,
+      "id": 11
+    },
+    {
+      "area": "顺义区",
+      "areaID": "110113",
+      "city": null,
+      "id": 12
+    },
+    {
+      "area": "昌平区",
+      "areaID": "110114",
+      "city": null,
+      "id": 13
+    },
+    {
+      "area": "大兴区",
+      "areaID": "110115",
+      "city": null,
+      "id": 14
+    },
+    {
+      "area": "怀柔区",
+      "areaID": "110116",
+      "city": null,
+      "id": 15
+    },
+    {
+      "area": "平谷区",
+      "areaID": "110117",
+      "city": null,
+      "id": 16
+    }
+  ],
+  "cartCon": {
+    "selectCartOfUser": [
+      {
+        "cartId": 1,
+        "goodList": [
+          {
+            "cartFlag": 0,
+            "cartNumber": 1,
+            "freeDelivery": "",
+            "goodDesc": "妙雀咖啡机商用全自动咖啡饮料机冷热 速溶咖啡奶茶一体机热饮机",
+            "goodDiscountedPrice": 2550,
+            "goodId": 1,
+            "goodName": "咖啡机",
+            "goodOriginalPrice": 0,
+            "goodPicPath": "https://gd1.alicdn.com/imgextra/i4/2804539712/TB2DtsWqRNkpuFjy0FaXXbRCVXa_!!2804539712.jpg",
+            "goodStatus": "",
+            "salsNum": 0,
+            "serviceType": [],
+            "specification": "台",
+            "styleId": 0
+          }
+        ],
+        "orderId": 0,
+        "realTotal": 0,
+        "total": 0,
+        "user": {
+          "cardno": "",
+          "email": "",
+          "frontid": 1,
+          "loginName": "",
+          "loginpwd": "",
+          "mobilePhone": "",
+          "nickName": "",
+          "openid": "",
+          "picPath": "",
+          "realName": "",
+          "userDesc": "",
+          "userStatus": ""
+        }
+      },
+      {
+        "cartId": 3,
+        "goodList": [
+          {
+            "cartFlag": 0,
+            "cartNumber": 1,
+            "freeDelivery": "",
+            "goodDesc": "发烧hifi书架音箱三四分频10寸木质监听无源音响震撼低音SP高保真",
+            "goodDiscountedPrice": 1814.4,
+            "goodId": 2,
+            "goodName": "HIFI音箱",
+            "goodOriginalPrice": 0,
+            "goodPicPath": "https://g-search1.alicdn.com/img/bao/uploaded/i4/imgextra/i4/198000345864446200/TB2Nnwlbb0kpuFjy0FjXXcBbVXa_!!0-saturn_solar.jpg_230x230.jpg_.webp",
+            "goodStatus": "",
+            "salsNum": 0,
+            "serviceType": [],
+            "specification": "对",
+            "styleId": 0
+          }
+        ],
+        "orderId": 0,
+        "realTotal": 0,
+        "total": 0,
+        "user": {
+          "cardno": "",
+          "email": "",
+          "frontid": 1,
+          "loginName": "",
+          "loginpwd": "",
+          "mobilePhone": "",
+          "nickName": "",
+          "openid": "",
+          "picPath": "",
+          "realName": "",
+          "userDesc": "",
+          "userStatus": ""
+        }
+      }
+    ]
+  }
+}
